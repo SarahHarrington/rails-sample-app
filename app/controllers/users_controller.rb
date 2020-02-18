7e260e3d424ddd570e4a9dabbf2bca3547d8e1b1
Sarah Harrington <sarah.harrington@iorahealth.com>
2020-02-18 10:13:04 -0600

    email templates for account activation set up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,10 +22,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      # rails autmoatically infers that we want to go to the user_url
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
